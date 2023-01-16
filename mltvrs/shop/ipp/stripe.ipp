8e92bfac5374eb4c81a755aa6eb46fc920f83551
Zuodian Hu <zhu48@wisc.edu>
2023-01-16 10:16:08 -0600

    feat(shop): `stripe::api_key`

---
+++ mltvrs/shop/ipp/stripe.ipp
@@ -0,0 +1,39 @@
+
+constexpr mltvrs::shop::stripe::api_key::api_key(mode op_mode, rfc4648_literal_type key) noexcept
+    : m_key{key.data(), key.size()},
+      m_mode{op_mode}
+{
+    Ensures(m_key.size() == m_key.capacity());
+}
+
+constexpr mltvrs::shop::stripe::api_key::api_key(
+    mode                       op_mode,
+    const rfc4648_string_type& key) noexcept
+    : m_key{key},
+      m_mode{op_mode}
+{
+    Ensures(m_key.size() == m_key.capacity());
+}
+
+[[nodiscard]] constexpr auto mltvrs::shop::stripe::api_key::hex_digits() const noexcept
+    -> rfc4648_string_type
+{
+    return {m_key.data(), m_key.capacity()};
+}
+
+constexpr void mltvrs::shop::stripe::api_key::hex_digits(rfc4648_literal_type key) noexcept
+{
+    m_key.assign(key.data(), key.size());
+}
+
+[[nodiscard]] constexpr auto mltvrs::shop::stripe::api_key::full_string() const noexcept
+    -> full_string_type
+{
+    auto ret =
+        (deploy_mode() == mode::test) ? full_string_type{"sk_test_"} : full_string_type{"sk_live_"};
+    ret += m_key;
+
+    Ensures(ret.size() == ret.capacity());
+
+    return ret;
+}
--- mltvrs/shop/stripe.hpp
+++ mltvrs/shop/stripe.hpp
@@ -1,6 +1,44 @@
 #pragma once
 
-#include <boost/json.hpp>
+#include <span>
+
+#include <boost/static_string.hpp>
+
+#include <mltvrs/string_literal.hpp>
 
 namespace mltvrs::shop::stripe {
-}
+
+    class api_key
+    {
+        public:
+            static constexpr auto rfc4648_chars = 99;
+            static constexpr auto prefix_chars  = 8;
+
+            using rfc4648_literal_type = string_literal<rfc4648_chars>;
+            using rfc4648_string_type  = boost::static_string<rfc4648_chars>;
+            using full_string_type     = boost::static_string<rfc4648_chars + prefix_chars>;
+
+            enum class mode {
+                test,
+                live
+            };
+
+            constexpr api_key(mode op_mode, rfc4648_literal_type key) noexcept;
+            constexpr api_key(mode op_mode, const rfc4648_string_type& key) noexcept;
+
+            [[nodiscard]] constexpr auto deploy_mode() const noexcept { return m_mode; }
+            constexpr void               deploy_mode(mode op_mode) noexcept { m_mode = op_mode; }
+            // NOLINTBEGIN(bugprone-exception-escape): string sizes fixed by design
+            [[nodiscard]] constexpr auto hex_digits() const noexcept -> rfc4648_string_type;
+            constexpr void               hex_digits(rfc4648_literal_type key) noexcept;
+            [[nodiscard]] constexpr auto full_string() const noexcept -> full_string_type;
+            // NOLINTEND(bugprone-exception-escape)
+
+        private:
+            rfc4648_string_type m_key;
+            mode                m_mode;
+    };
+
+} // namespace mltvrs::shop::stripe
+
+#include <mltvrs/shop/ipp/stripe.ipp>
