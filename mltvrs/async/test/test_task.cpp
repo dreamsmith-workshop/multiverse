3e889b129df499a96bc0b29ea1efa2bedf9780d2
Zuodian Hu <zhu48@wisc.edu>
2023-02-02 12:59:05 -0600

    test(async): all synchronous tests

--- mltvrs/async/test/test_task.cpp
+++ mltvrs/async/test/test_task.cpp
@@ -1,3 +1,5 @@
+#include <stop_token>
+
 #include <mltvrs/async/task.hpp>
 
 #include <catch2/catch_all.hpp>
@@ -25,16 +27,38 @@ CATCH_SCENARIO("synchronously waiting on a task produces the correct result")
 
     CATCH_GIVEN("a task that performs a calculation with internal yields")
     {
+        auto       ssource   = std::stop_source{};
+        const auto task_coro = [token = ssource.get_token()](int arg) -> mltvrs::async::task<int>
+        {
+            while(!token.stop_requested()) {
+                co_await std::suspend_always{};
+            }
+
+            co_return arg;
+        };
+
         CATCH_WHEN("that task is executed without the completion criteria being met")
         {
+            const auto data = GENERATE(take(5, random(-100, 100)));
+
+            auto task = task_coro(data);
+
             CATCH_THEN("that task does not become ready")
             {
+                CATCH_REQUIRE(task);
+                CATCH_REQUIRE(!task.ready());
             }
 
             CATCH_AND_WHEN("that task is executed with the completion criteria met")
             {
+                ssource.request_stop();
+                task();
+
                 CATCH_THEN("that task becomes ready, and returns the correct value")
                 {
+                    CATCH_REQUIRE(!task);
+                    CATCH_REQUIRE(task.ready());
+                    CATCH_REQUIRE(task.get() == data);
                 }
             }
         }
