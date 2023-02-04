b9306d9218cae9c35febfeda2b72e6dbbe61049d
Zuodian Hu <zhu48@wisc.edu>
2023-02-03 20:52:48 -0600

    test(async): no asynchronous tests for `task`

--- mltvrs/async/test/test_task.cpp
+++ mltvrs/async/test/test_task.cpp
@@ -4,7 +4,7 @@
 
 #include <catch2/catch_all.hpp>
 
-CATCH_SCENARIO("synchronously waiting on a task produces the correct result")
+CATCH_SCENARIO("waiting on a task produces the correct result")
 {
     CATCH_GIVEN("a task that performs a calculation without yielding in the middle")
     {
@@ -61,36 +61,6 @@ CATCH_SCENARIO("synchronously waiting on a task produces the correct result")
                     CATCH_REQUIRE(!task);
                     CATCH_REQUIRE(task.ready());
                     CATCH_REQUIRE(task.get() == data);
-                }
-            }
-        }
-    }
-}
-
-CATCH_SCENARIO("asynchronously waiting on a task produces the correct result")
-{
-    CATCH_GIVEN("a task that performs a calculation without yielding in the middle")
-    {
-        CATCH_WHEN("that task is posted onto an executor")
-        {
-            CATCH_THEN("that task blocks the awaiter until it completes with the correct value")
-            {
-            }
-        }
-    }
-
-    CATCH_GIVEN("a task that performs a calculation with internal yields")
-    {
-        CATCH_WHEN("that task is posted onto an executor without the completion criteria being met")
-        {
-            CATCH_THEN("that task blocks the awaiter")
-            {
-            }
-
-            CATCH_AND_WHEN("that task has its completion criteria met")
-            {
-                CATCH_THEN("that task becomes ready, and returns the correct value")
-                {
                 }
             }
         }
